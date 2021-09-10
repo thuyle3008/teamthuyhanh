07f307945c520159c665234fa080eccb3531eaf4
HanhNguyen319 <nguyen.thuyhanh319@gmail.com>
2021-09-10 10:59:41 +0700

    Hạnh commit lần 4, tạo TC, TestSuite

---
+++ Test Suites/CreateForCommitSuite.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>CreateForCommitSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>bbcbc024-9488-4be2-861e-024e67007401</testSuiteGuid>
+   <testCaseLink>
+      <guid>aae49585-d113-4d77-9957-ac7dcf9d2dbf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CreateForCommitTest</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>fd04770d-0907-43af-8b4b-f915064cd88f</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
